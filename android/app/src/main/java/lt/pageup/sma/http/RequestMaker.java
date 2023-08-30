a04fcd7d50e3770670f4177bac52aa218b6ad72f
AdomasDauda <107749872+0xleft@users.noreply.github.com>
2023-08-31 00:23:21 +0100

    ye

--- android/app/src/main/java/lt/pageup/sma/http/RequestMaker.java
+++ android/app/src/main/java/lt/pageup/sma/http/RequestMaker.java
@@ -1,29 +1,57 @@
 package lt.pageup.sma.http;
 
+import java.io.DataOutputStream;
+import java.net.URL;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+
+import lt.pageup.sma.objects.Message;
+
 public class RequestMaker {
 
-    public static boolean register(String phoneNumber, String secretString, String publicKey) {
+    //private static final String BASE_URL = "https://pageup.lt/api/sma";
+    private static final String BASE_URL = "http://localhost:5000";
 
-        // /auth/register
-        // TODO make request to api to register user
-        return true;
+    public static boolean register(String phoneNumber, String secretString, String publicKey) {
+        return postRequest("/auth/register", "{\"phoneNumber\":\"" + phoneNumber + "\",\"secretString\":\"" + secretString + "\",\"publicKey\":\"" + publicKey + "\"}");
     }
 
     public static boolean sendMessage(String phoneNumber, String toPhoneNumber, String secretString, String encryptedMessage) {
-        // /messages/send
-        // TODO make request to api to send message to user
-        return true;
+        return postRequest("/messages/send", "{\"phoneNumber\":\"" + phoneNumber + "\",\"secretString\":\"" + secretString + "\",\"to\":\"" + toPhoneNumber + "\",\"message\":\"" + encryptedMessage + "\"}");
     }
 
-    public static boolean getMessages(String phoneNumber, String secretString) {
-        // /messages
-        // TODO make request to api to get message of a user
-        return true;
+    public static List<Message> getMessages(String phoneNumber, String secretString) {
     }
 
-    public static boolean getPublicKey(String phoneNumber) {
-        // /auth/get_public_key
-        // TODO make request to api to get public key of a user
-        return true;
+    public static String getPublicKey(String phoneNumber) {
+
+    }
+
+    public static boolean postRequest(String location, String data) {
+        URL url = new URL(BASE_URL + location);
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, null, new java.security.SecureRandom());
+        connection.setSSLSocketFactory(sslContext.getSocketFactory());
+
+
+        connection.setRequestMethod("POST");
+        connection.setConnectTimeout(5000);
+        connection.setRequestProperty("User-Agent", "sma user agent");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
+
+        // Write JSON data to the request
+        try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+            wr.writeBytes(data);
+            wr.flush();
+        }
+
+        int responseCode = connection.getResponseCode();
+
+        return responseCode == 200;
     }
 }
