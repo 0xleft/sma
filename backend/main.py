52d6e712e5151118124f992b98c72fd13a8bbc54
AdomasDauda <107749872+0xleft@users.noreply.github.com>
2023-09-01 21:54:03 +0100

    update backend since its out of sync

--- backend/main.py
+++ backend/main.py
@@ -1,43 +1,34 @@
-import flask
-import pymongo
-import os
-import waitress
-
 import pymongo
 import flask
+import hashlib
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["sma"]
 
-app = flask.Flask(__name__)
+api = flask.Blueprint("sma", __name__)
 
-app.secret_key = os.urandom(10000)
-
-client = pymongo.MongoClient("mongodb://mongodb:27017/")
-db = client["main"]
-
-@app.route("/api/sma/messages", methods=["GET"])
+@api.route("/api/sma/messages", methods=["GET"])
 def get_messages():
 
     phone_number = flask.request.args.get("phoneNumber")
-    secret_string = flask.request.args.get("secretString")
+    secret_string = flask.request.headers.get("secretString")
 
-    user = db["users"].find_one({"phoneNumber": phone_number, "secretString": secret_string})
+    user = db["users"].find_one({"phoneNumber": phone_number, "secretString": hashlib.sha256(secret_string).hexdigest()})
     if user is None:
         return flask.jsonify({"error": "Invalid credentials"}), 401
     
     messages = db["messages"].find({"to": phone_number})
+
+    messages_out = [{
+        "from": message["from"],
+        "message": message["message"]
+    } for message in messages]
+
     db["messages"].delete_many({"to": phone_number})
 
-    return flask.jsonify({
-        "messages": [{
-            "from": message["from"], 
-            "to": message["to"],
-            "message": message["message"],
-        } for message in messages]
-    })
+    return flask.jsonify({"messages": messages_out})
 
-@app.route("/api/sma/messages/send", methods=["POST"])
+@api.route("/api/sma/messages/send", methods=["POST"])
 def send_message():
     data = flask.request.get_json(silent=True, force=True)
     if data is None:
@@ -51,7 +42,7 @@ def send_message():
     message = str(data["message"])
     to_number = str(data["to"])
 
-    user = db["users"].find_one({"phoneNumber": phone_number, "secretString": secret_string})
+    user = db["users"].find_one({"phoneNumber": phone_number, "secretString": hashlib.sha256(secret_string).hexdigest()})
     if user is None:
         return flask.jsonify({"error": "Invalid credentials"}), 403
     
@@ -63,7 +54,7 @@ def send_message():
 
     return flask.jsonify({"success": True})
 
-@app.route("/api/sma/auth/register", methods=["POST"])
+@api.route("/api/sma/auth/register", methods=["POST"])
 def register():
     data = flask.request.get_json(silent=True, force=True)
     if data is None:
@@ -82,20 +73,17 @@ def register():
     db["users"].insert_one({
         "phoneNumber": phone_number,
         "publicKey": public_key,
-        "secretString": secret_string,
+        "secretString": hashlib.sha256(secret_string).hexdigest(),
     })
 
     return flask.jsonify({"success": True})
 
-@app.route("/api/sma/auth/get_public_key", methods=["GET"])
+@api.route("/api/sma/auth/get_public_key", methods=["GET"])
 def get_public_key():
     phone_number = flask.request.args.get("phoneNumber")
 
     user = db["users"].find_one({"phoneNumber": phone_number})
     if user is None:
-        return flask.jsonify({"error": "User does not exist"}), 404
+        return flask.jsonify({"error": "User does not exist"}), 200
     
     return flask.jsonify({"publicKey": user["publicKey"]})
-
-if __name__ == "__main__":
-    waitress.serve(app, port=5000)
